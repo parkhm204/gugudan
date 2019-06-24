cee8182c67ac97ac28dccb4b3037c2f0b0091f88
jlee <jlee@xevo.com>
2019-06-24 22:41:23 +0900

    gugudan

---
+++ gugudan/src/gugudan/gugudan.java
@@ -0,0 +1,23 @@
+package gugudan;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		
+	
+	
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2 * 4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*1);
+		System.out.println(2*1);
+
+
+		System.out.println(2*1);
+	
+	
+	}
+	}
